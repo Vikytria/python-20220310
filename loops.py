5be169a8e32daf800110d21e517371892c4d57da
Vikytria <viky.tria@gmail.com>
2022-03-10 15:08:39 +0100

    Loop def

---
+++ loops.py
@@ -0,0 +1,19 @@
+
+for i in range(5):
+    print("Haliho :D Hello Python!")
+    print(i)
+print("End")
+
+for i in range(6, 10):      # [6, 7, 8, 9]
+    print(i)
+print("End")
+
+for i in [2, 7, 11, 22]:      # [6, 7, 8, 9]
+    print(i)
+print("End")
+
+for name in ["John", "Jack", "Jane"]:
+    print(name)
+
+
+
