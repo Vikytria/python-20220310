a62004bc12688e86442fdb8d5a5f5389d5fd6eef
Vikytria <viky.tria@gmail.com>
2022-03-10 15:37:29 +0100

    ciklusok - summa

---
+++ input_loop.py
@@ -0,0 +1,21 @@
+
+#name = input("Mi a neved?")
+#loop = int(input("Hányszor írjam ki?"))
+
+#for i in range(loop):
+    #print(f"{i + 1}.{name}")
+
+months = ["Január", "Február", "Március", "Április"]
+for month in months:
+    print(f"Az év egyik hónapja {month}.")
+print("stb.")
+
+numbers = [6, 5, 4, 3, 9]
+sum = 0
+for number in numbers:
+    #sum = sum + number
+    sum += number
+print(sum)
+
+
+
