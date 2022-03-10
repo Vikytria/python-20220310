74296d11154762c7d3d91c8b756ea1d64d14fb8a
Vikytria <viky.tria@gmail.com>
2022-03-10 14:27:18 +0100

    Kifejezések

---
+++ expressions.py
@@ -0,0 +1,20 @@
+
+print(2 ** 3)
+print(10 // 3)
+print(10 % 3)
+
+#  typus konverziós függvény - levágja a tizedest
+print(int(4.3))
+print(int(4.9))
+
+# Precedencia
+i = 7 + 3
+i = 7 + 3 * 2
+i = (7 + 3) * 2
+
+j = 4
+i = (7 + (i * j)) * len("alma")
+print(i)
+
+print("X" * 3)
+
