61b69dc0b7c342e28f19545bf4bfae54d9ff0f2b
Vikytria <viky.tria@gmail.com>
2022-03-10 14:41:15 +0100

    Input def

---
+++ consol.py
@@ -0,0 +1,12 @@
+# Adat bekérés
+
+# name = input("Mi a neved?")
+# print(name)
+# print(type(name))
+
+# user_age = input("Hány éves vagy?")
+# print(int(user_age) * 2)
+
+age = int(input("Hány éves vagy?"))
+print(age * 2)
+
