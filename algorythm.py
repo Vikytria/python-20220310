23daf2706531004022fa5af9f2ce9b9ad2162def
Vikytria <viky.tria@gmail.com>
2022-03-11 11:23:14 +0100

    Javítás

--- algorythm.py
+++ algorythm.py
@@ -5,16 +5,26 @@ min = 1
 max = 10
 
 answer = "x"
+prevguess = -1
+guess = -2
 
 while answer!= "e":
+    prevguess = guess
     guess = (min + max) // 2
-    print(f"""Erre a számra gondoltam: {guess}""")
-
-    answer = input("Kisebb (k), nagyobb(n) vagy egyenlő(e)?")
-
-    if answer == "k":
-        max = guess
-    elif answer == "n":
-        min = guess
+    
+    if guess == prevguess: 
+        print("Ne szórakozz velem!")
+        answer = "e" or "E"
     else:
-        print(f"""{guess}-ra gondoltál""")
+        print(f"""Erre a számra gondoltam: {guess}""")
+
+        answer = input("Kisebb (k), nagyobb(n) vagy egyenlő(e)?")
+
+        if answer == "k" or answer == "K":
+            max = guess
+        elif answer == "n" or answer == "N":
+            min = guess
+        elif answer == "e" or answer == "E":
+            print(f"""{guess}-ra gondoltál""")
+        else: 
+            print("Ez nem jó válasz!")
