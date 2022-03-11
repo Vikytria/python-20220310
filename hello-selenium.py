2db9f6a61b2f43e1663a87d4fddd0db6019aba4d
Vikytria <viky.tria@gmail.com>
2022-03-11 09:36:39 +0100

    Áttérés Codera

--- hello-selenium.py
+++ hello-selenium.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://python.org")
-driver.find_element(By.ID, "id-search-field").send_keys("precedence")
+driver.find_element(By.ID, "id-search-field").send_keys("Visual Studio Code")
 driver.find_element(By.ID, "submit").click()
 
 
