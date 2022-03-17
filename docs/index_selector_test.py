325f57d32999082aebf45ef57611095bdead1d09
Vikytria <viky.tria@gmail.com>
2022-03-17 15:49:36 +0100

    css selecctor

--- docs/index_selector_test.py
+++ docs/index_selector_test.py
@@ -7,7 +7,7 @@ def test_relative_selector(driver: WebElement):
     driver.get("http://127.0.0.1:5500/docs/index.html")
     form = driver.find_element(By.ID, "name-form")  
 
-    input:WebElement = form.find_element(By.CSS_SELECTOR, "[name ='name']")
+    input:WebElement = form.find_element(By.CSS_SELECTOR, "[name ='name']")                 #gyorsítja a tesztek elvégzését azzal h megmondjuk hol keressen 
     button:WebElement= form.find_element(By.CSS_SELECTOR, "[value = 'Say welcome!']")
 
     input.send_keys("John Doe")
